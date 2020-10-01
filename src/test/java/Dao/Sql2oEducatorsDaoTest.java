37f9bf68d9afa9ad7f494450f335dacea501d016
Ken-muigai <kenmuigai980@gmail.com>
2020-10-01 19:35:19 +0300

    added tests

---
+++ src/test/java/Dao/Sql2oEducatorsDaoTest.java
@@ -0,0 +1,17 @@
+package Dao;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class Sql2oEducatorsDaoTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
