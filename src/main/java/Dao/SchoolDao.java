04602168719ce95ac201c43b10289a234fb2b760
Ken-muigai <kenmuigai980@gmail.com>
2020-10-01 20:38:36 +0300

    schema created

--- src/main/java/Dao/SchoolDao.java
+++ src/main/java/Dao/SchoolDao.java
@@ -1,4 +1,27 @@
 package Dao;
 
+import models.Schools;
+import models.Students;
+
+import java.util.List;
+
 public interface SchoolDao {
+
+    //create
+    void add(Schools schools);
+
+
+    //read
+    Schools findSchoolById(int schoolId);
+    List<Schools> getAll();
+    List<Students> getAllStudentsBySchool(int id);
+
+    //update
+    //void update(int id, String schoolName, String schoolAddress, String schoolWebsite, String schoolEmail, String phoneNumber);
+
+
+    //delete
+    void deleteById(int id);
+    void clearAll();
+
 }
